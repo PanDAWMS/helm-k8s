dca55e32263bdaccdbe9bb3f0082d4fac09ed204
Handson-peng <han-sheng.peng@cern.ch>
2020-04-19 13:32:09 +0000

    install htcondor package

--- harvester/docker/harvester_start.sh
+++ harvester/docker/harvester_start.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+source /usr/etc/sysconfig/panda_harvester
+DB_USER=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.user)'`
+DB_PASSWORD=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.password)'`
+DB_HOST=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.host)'`
+HARVESTER_UID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.uname)'`
+HARVESTER_GID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.gname)'`
+groupadd ${HARVESTER_GID}
+useradd -g ${HARVESTER_GID} ${HARVESTER_UID}
+chown -R ${HARVESTER_UID}:${HARVESTER_GID} /var/log/panda
+chown -R ${HARVESTER_UID}:${HARVESTER_GID} /var/log/harvester
+chown -R ${HARVESTER_UID}:${HARVESTER_GID} /harvester_wdirs
+echo ${HARVESTER_UID} "ALL = (root) NOPASSWD:ALL" > /etc/sudoers.d/${HARVESTER_UID}
+while true
+do
+  mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} -e '\q' && break
+  sleep 5
+done
 while true;
 do
   /usr/etc/rc.d/init.d/panda_harvester-uwsgi start

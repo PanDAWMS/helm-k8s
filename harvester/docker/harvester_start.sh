6211b00cece2ad9f2e26ab6d827d38988654a59c
Handson-peng <han-sheng.peng@cern.ch>
2020-06-11 16:42:24 +0000

    add condor_example

--- harvester/docker/harvester_start.sh
+++ harvester/docker/harvester_start.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 source /usr/etc/sysconfig/panda_harvester
-DB_USER=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.user)'`
-DB_PASSWORD=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.password)'`
-DB_HOST=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.host)'`
-HARVESTER_UID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.uname)'`
-HARVESTER_GID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.gname)'`
+export HARVESTER_UNAME=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.uname)'`
+export HARVESTER_GNAME=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.gname)'`
+export HARVESTER_UID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.uid)'`
+export HARVESTER_GID=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.master.gid)'`
 #/usr/bin/mysqld_safe --datadir='/var/lib/mysql' --port=3306 --nowatch
-groupadd ${HARVESTER_GID}
-useradd -g ${HARVESTER_GID} ${HARVESTER_UID}
+groupadd -g ${HARVESTER_GID} ${HARVESTER_GNAME}
+useradd -g ${HARVESTER_GID} -u ${HARVESTER_UID} ${HARVESTER_UNAME}
 chown -R ${HARVESTER_UID}:${HARVESTER_GID} /var/log/panda
 chown -R ${HARVESTER_UID}:${HARVESTER_GID} /var/log/harvester
 chown -R ${HARVESTER_UID}:${HARVESTER_GID} /harvester_wdirs
 echo ${HARVESTER_UID} "ALL = (root) NOPASSWD:ALL" > /etc/sudoers.d/${HARVESTER_UID}
 while true
 do
-  mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} -e '\q' && break
+  DB_USER=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.user)'`
+  DB_PASSWORD=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.password)'`
+  DB_HOST=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.host)'`
+  DB_PORT=`python -c 'from pandaharvester.harvesterconfig import harvester_config;print(harvester_config.db.port)'`
+  mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} --password=${DB_PASSWORD} -e '\q' && break
   sleep 5
 done
 #mysql -e "create database ${DB_SCHEMA};" && \

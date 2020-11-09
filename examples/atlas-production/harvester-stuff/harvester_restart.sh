9f03dc9d7daf386d12ce45541ccfece9e7a13588
Han-Sheng Peng <hapeng@lxplus725.cern.ch>
2020-11-09 16:38:36 +0100

    fix crond daemon issue

--- examples/atlas-production/harvester-stuff/harvester_restart.sh
+++ examples/atlas-production/harvester-stuff/harvester_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source /usr/etc/sysconfig/panda_harvester 
 /usr/etc/rc.d/init.d/panda_harvester-uwsgi stop 
-logrotate /usr/etc/panda/harvester_logroate 
+/usr/sbin/logrotate /usr/etc/panda/harvester_logroate 
 /usr/etc/rc.d/init.d/panda_harvester-uwsgi start
